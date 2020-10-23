92f1c2ba8253392ddd5810fed9a2ad3c318a53f9
ChernenkoArtem <56413843+ChernenkoArtem@users.noreply.github.com>
2020-10-23 22:19:26 +0300

    Add files via upload

---
+++ precache-manifest.623cf01159cf90dc512d9408bb253de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b212450514497781a7ae6eacac128b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27ce4406d5c784c7598f",
+    "url": "/static/css/main.45661148.chunk.css"
+  },
+  {
+    "revision": "8c3e44f500531509eda9",
+    "url": "/static/js/2.334b25c8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.334b25c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ce4406d5c784c7598f",
+    "url": "/static/js/main.26e68379.chunk.js"
+  },
+  {
+    "revision": "c4350b401c716726543a",
+    "url": "/static/js/runtime-main.a84afe84.js"
+  }
+]);
